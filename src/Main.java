193ad1da0c1ebb1b091f6656ca05d77502ad4530
Alexdead21 <matveev123-00@mail.ru>
2023-12-11 21:42:17 +0400

    BUGFIX 1.0

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,31 @@ public class Main {
     public static String calculate(String input) throws Exception {
 
 
-            String[] strings = input.split(" ");
-            if (strings.length > 3) {
-                throw new Exception();
-            }
-            String num1str = strings[0];
-            String num2str = strings[2];
-            String action = strings[1];
+        String[] strings = input.split(" ");
+        if (strings.length > 3) {
+            throw new Exception();
+        }
+        String num1str = strings[0];
+        String num2str = strings[2];
+        String action = strings[1];
 
 
-            int num1;
-            int num2;
-            int result;
+        int num1;
+        int num2;
+        int result;
 
-            boolean isRoman = false;
-            try {
-                num1 = Integer.parseInt(num1str);
-                num2 = Integer.parseInt(num2str);
-            } catch (NumberFormatException e) {
-                num1 = stringToRoman(num1str);
-                num2 = stringToRoman(num2str);
-                isRoman = true;
-            }
-            if ((num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10)) {
-                throw new Exception();
-            }
+        boolean isRoman = false;
+        try {
+            num1 = Integer.parseInt(num1str);
+            num2 = Integer.parseInt(num2str);
+        } catch (NumberFormatException e) {
+            num1 = stringToRoman(num1str);
+            num2 = stringToRoman(num2str);
+            isRoman = true;
+        }
+        if ((num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10)) {
+            throw new Exception();
+        }
         result = switch (action) {
             case "+" -> num1 + num2;
             case "-" -> num1 - num2;
@@ -42,14 +42,14 @@ public class Main {
             case "/" -> num1 / num2;
             default -> throw new Exception();
         };
-            if (!isRoman) {
-            } else {
-                if (result < 0) {
-                    throw new Exception();
-                }
+        if (isRoman) {
+            if (result < 1) {
+                throw new Exception();
             }
+            return integerToRoman(result);
+        } else {
             return String.valueOf(result);
-
+        }
     }
 
     private static int stringToRoman(String num) throws Exception {
